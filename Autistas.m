a6db200ed206340896bbcce13ae13b95e3b5c013
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-02-28 14:49:32 -0300

    Proporção de genero e paises entre os Autistas

--- Autistas.m
+++ Autistas.m
@@ -3,31 +3,57 @@
 % Data Science
 % Script para analisar os participantes que possuem Autismo
 
+%Verificar quais participantes possuem autismo
 j=1;num_Amostras=length(ClassASD); %Variaveis auxiliares
-for i=1:num_Amostras
-    if strncmp(ClassASD(i,:),'YES',3)
-        Autista(j) = i;
-        j=j+1;
+for i=1:num_Amostras %para todos os participantes
+    if strncmp(ClassASD(i,:),'YES',3) %se ele tiver autismo
+        Autista(j) = i; %Coloca o indice no mesmo em Autista
+        j=j+1; %acrescenta j
     end
 end
+%Aki temos o vetor Autista com os indices dos Autistas
 
-f=0;m=0;tam=length(Autista);
-for i=1:tam
-    if isequal(gender(i),'m' )
-        m = m+1;
+%Sexo dos Autistas
+%Seçao para analisar caracteristicas relacionando o genero
+
+f=0;m=0; %contador feminino e masculino
+tam=length(Autista); %numero de autistas
+
+for i=1:tam %para todos os autistas
+    if isequal(gender(Autista(i)),'m' ) %se o Autista for masculino
+        m = m+1; %incrementa m
     else
-        if isequal(gender(i),'f' )
-            f=f+1;
+        if isequal(gender(Autista(i)),'f' ) %se o Autista for feminino
+            f=f+1; %incrementa m
         end
     end
 end
+%Neste ponto sabemos a proporção do sexo dos Autistas
 
-fprintf('A percentagem de Autistas do sexo feminino é: %.2f\n',f*100/tam);%exibe o percentual de acertos
-fprintf('A percentagem de Autistas do sexo masculino é: %.2f\n',m*100/tam);%exibe o percentual de acertos
+fprintf('A percentagem de Autistas do sexo feminino é: %.2f\n',f*100/tam);%exibe o percentual
+fprintf('A percentagem de Autistas do sexo masculino é: %.2f\n',m*100/tam);%exibe o percentual
 
 codelines = [m f];
 coders = {'Masculino','Feminino'};
 
 figure
-pareto(codelines, coders)
-title('Publico Participante')
+pareto(codelines, coders) %plota o grafico com quantidade e percentagem de sexo dos Autistas
+title('Participante Autistas')
+
+%Paises dos Autistas
+%Seçao para analisar caracteristicas relacionando o pais de residencia
+
+%Cria tabela com paises na primeira colouna e numero de concorrencia na
+%segunda
+tabela2 = tabulate(contry_of_res)
+
+codelines = cell2mat(tabela2(:,2));
+coders = char(tabela2(:,1));
+
+figure
+pareto(codelines, coders)%plota o grafico com quantidade dos paises
+%Obs.: Devido o numero de paises presente pode ser enteressante visualizar
+%estes dados em mais de um grafico
+title('Autistas por Paises')
+xlabel('Paises')
+ylabel('Quantidade')
