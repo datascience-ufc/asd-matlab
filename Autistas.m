c9fcf92a2113d1de930e522db64072a4d0f42a9e
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-03-01 17:19:46 -0300

    Analise relativa a gênero, paéses, parente autista e Icteria realizada

--- Autistas.m
+++ Autistas.m
@@ -4,11 +4,14 @@
 % Script para analisar os participantes que possuem Autismo
 
 %Verificar quais participantes possuem autismo
-j=1;num_Amostras=length(ClassASD); %Variaveis auxiliares
+j=1;z=1;num_Amostras=length(ClassASD); %Variaveis auxiliares
 for i=1:num_Amostras %para todos os participantes
     if strncmp(ClassASD(i,:),'YES',3) %se ele tiver autismo
-        Autista(j) = i; %Coloca o indice no mesmo em Autista
+        Autista(j) = i; %Coloca o indice do mesmo em Autista
         j=j+1; %acrescenta j
+    else %se ele não tiver autismo
+        naoAutista(z) = i; %Coloca o indice do mesmo em naoAutista
+        z=z+1; %acrescenta z
     end
 end
 %Aki temos o vetor Autista com os indices dos Autistas
@@ -45,10 +48,11 @@ title('Participante Autistas')
 
 %Cria tabela com paises na primeira colouna e numero de concorrencia na
 %segunda
-tabela2 = tabulate(contry_of_res)
+tabela3 = tabulate(contry_of_res);
+%Neste ponto a proporção dos paises dos autistas
 
-codelines = cell2mat(tabela2(:,2));
-coders = char(tabela2(:,1));
+codelines = cell2mat(tabela3(:,2));
+coders = char(tabela3(:,1));
 
 figure
 pareto(codelines, coders)%plota o grafico com quantidade dos paises
@@ -57,3 +61,66 @@ pareto(codelines, coders)%plota o grafico com quantidade dos paises
 title('Autistas por Paises')
 xlabel('Paises')
 ylabel('Quantidade')
+
+%Parentesco dos Autistas
+%Seçao para verificar se o autista possui algum parente com autismo
+
+tabela4 = tabulate(austim(Autista,:));
+%Neste ponto quais autistas possuem parente autista
+
+fprintf('A percentagem de Autistas que possuim parente autista é: %.2f\n',cell2mat(tabela4(2,3)));%exibe o percentual
+fprintf('A percentagem de Autistas que não possuim parente autista é: %.2f\n',cell2mat(tabela4(1,3)));%exibe o percentual
+
+codelines = [cell2mat(tabela4(2,3)) cell2mat(tabela4(1,3))];
+coders = {'Parente-Autista','sem Parente-Autista'};
+
+figure
+pareto(codelines, coders) %plota o grafico com quantidade e percentagem
+title('Autista')
+
+%Seçao para verificar se o nao-autista possui algum parente com autismo
+
+tabela5 = tabulate(austim(naoAutista,:));
+%Neste ponto quais noa-autistas possuem parente autista
+
+fprintf('A percentagem de não-Autistas que possuim parente autista é: %.2f\n',cell2mat(tabela5(2,3)));%exibe o percentual
+fprintf('A percentagem de não-Autistas que não possuim parente autista é: %.2f\n',cell2mat(tabela5(1,3)));%exibe o percentual
+
+codelines = [cell2mat(tabela5(2,3)) cell2mat(tabela5(1,3))];
+coders = {'Parente-Autista','sem Parente-Autista'};
+
+figure
+pareto(codelines, coders) %plota o grafico com quantidade e percentagem
+title('Não-Autista')
+
+
+%Doença de pigmentação Icteria
+%Seçao para verificar se o autista tem Icteria
+
+tabela6 = tabulate(jundice(Autista,:));
+%Neste ponto sabemos a proporção Autistas que possuem Icteria
+
+fprintf('A percentagem de Autistas que possuim Icteria é: %.2f\n',cell2mat(tabela6(1,3)));%exibe o percentual
+fprintf('A percentagem de Autistas que não possuim Icteria é: %.2f\n',cell2mat(tabela6(2,3)));%exibe o percentual
+
+codelines = [cell2mat(tabela6(2,3)) cell2mat(tabela6(1,3))];
+coders = {'sem Icteria','com Icteria'};
+
+figure
+pareto(codelines, coders) %plota o grafico com quantidade e percentagem
+title('Autistas')
+
+%Seçao para verificar se o nao-autista tem Icteria
+
+tabela7 = tabulate(jundice(naoAutista,:));
+%Neste ponto sabemos a proporção nao-Autistas que possuem Icteria
+
+fprintf('A percentagem de não-autistas que possuim Icteria é: %.2f\n',cell2mat(tabela7(2,3)));%exibe o percentual
+fprintf('A percentagem de não-autistas que não possuim Icteria é: %.2f\n',cell2mat(tabela7(1,3)));%exibe o percentual
+
+codelines = [cell2mat(tabela7(1,3)) cell2mat(tabela7(2,3))];
+coders = {'sem Icteria','com Icteria'};
+
+figure
+pareto(codelines, coders) %plota o grafico com quantidade e percentagem
+title('Não-Autistas')
